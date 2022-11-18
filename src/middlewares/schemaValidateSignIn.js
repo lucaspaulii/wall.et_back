ce9ab96885c4b6836db519e7bc05ee9cd28f0f2f
lucaspaulii <lucaspauli22@gmail.com>
2022-11-18 15:07:21 -0300

    fix: schemaValidateSignIn

--- src/middlewares/schemaValidateSignIn.js
+++ src/middlewares/schemaValidateSignIn.js
@@ -1,9 +1,23 @@
-import { sessionsCollection, usersCollection } from "../database/database.js";
+import {  usersCollection } from "../database/database.js";
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
+import joi from "joi";
 
-export async function validateSignIn(req, res, next) {
+const signInSchema = joi.object({
+  email: joi.string().email().required(),
+  password: joi.string().min(5).required(),
+});
+
+export async function schemaValidateSignIn(req, res, next) {
   const user = req.body;
+  const { error } = signInSchema.validate(user, {
+    abortEarly: false,
+  });
+  if (error) {
+    const errors = error.details.map((detail) => detail.message);
+    return res.status(422).send(errors);
+  }
+
   try {
     const emailExists = await usersCollection.findOne({ email: user.email });
     if (!emailExists) {
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { postSignIn, postSignUp } from '../controllers/usersControllers.js';
 import { schemaValidateSignUp } from '../middlewares/schemaValidateSignUp.js';
-import { validateSignIn } from '../middlewares/validateSignIn.js';
+import { schemaValidateSignIn } from '../middlewares/schemaValidateSignIn.js';
 
 const router = Router();
 
 router.post('/sign-up', schemaValidateSignUp, postSignUp);
 
-router.post('/sign-in', validateSignIn, postSignIn);
+router.post('/sign-in', schemaValidateSignIn, postSignIn);
 
 export default router
