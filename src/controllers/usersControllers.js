177b5cf2df2179602fcac98bba20ebc9786a2fb8
lucaspaulii <lucaspauli22@gmail.com>
2022-11-21 12:49:12 -0300

    feat: get user info

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -12,18 +12,33 @@ export async function postSignUp(req, res) {
 }
 
 export async function postSignIn(req, res) {
-  const user = req.sessionUser;      
+  const user = req.sessionUser;
   try {
     const tokenExists = await sessionsCollection.findOne({
       userID: user.userID,
     });
     if (tokenExists) {
       res.send(tokenExists.token);
-      return
+      return;
     }
     await sessionsCollection.insertOne(user);
     res.send(user.token);
   } catch (error) {
     res.sendStatus(400);
+  }
+}
+
+export async function getUserInfo(req, res) {
+  const userID = req.userID;
+
+  try {
+    const userInfo = await usersCollection.findOne({ _id: userID });
+    if (!userInfo) {
+      return res.sendStatus(404);
+    }
+    delete userInfo.password;
+    return res.send(userInfo);
+  } catch (error) {
+    return res.sendStatus(401);
   }
 }
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,12 +1,15 @@
 import { Router } from 'express';
-import { postSignIn, postSignUp } from '../controllers/usersControllers.js';
+import { getUserInfo, postSignIn, postSignUp } from '../controllers/usersControllers.js';
 import { schemaValidateSignUp } from '../middlewares/schemaValidateSignUp.js';
 import { schemaValidateSignIn } from '../middlewares/schemaValidateSignIn.js';
+import { validateToken } from '../middlewares/validateToken.js';
 
 const router = Router();
 
 router.post('/sign-up', schemaValidateSignUp, postSignUp);
 
 router.post('/sign-in', schemaValidateSignIn, postSignIn);
+
+router.get('/user', validateToken, getUserInfo);
 
 export default router
